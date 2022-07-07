a892287f5d1ded99a4a555f53cae81b143ad6307
COkedat <47716620+COkedat@users.noreply.github.com>
2022-07-07 21:22:57 +0900

    [Update] edit resources

--- app/src/main/java/com/example/receiving/MainActivity.kt
+++ app/src/main/java/com/example/receiving/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.receiving
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Switch
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +19,60 @@ class MainActivity : AppCompatActivity() {
 
         val tvDate = findViewById<TextView>(R.id.tv_date)
 
-        tvDate.text = "3일 후에 알람이 울려요"
+        // 최상위 부모에서 카드뷰로 연결
+        val cardAlert = findViewById<CardView>(R.id.card_alert)
+
+        //카드뷰에서 ConstraintLayout으로 연결
+        val cardCL = findViewById<ConstraintLayout>(R.id.card_cl)
+
+        // ConstraintLayout에서 스위치에 연결
+        val swCard = cardCL.findViewById<Switch>(R.id.swAlert)
+
+        val fabAdd = findViewById<FloatingActionButton>(R.id.fabAdd)
+
+        val tvTime = cardCL.findViewById<TextView>(R.id.tvTime)
+        val tvDay = cardCL.findViewById<TextView>(R.id.tvDay)
+        val tv_Date = findViewById<TextView>(R.id.tvDate)
+
+
+        if (swCard.isChecked){
+            tvTime.setTextColor(ContextCompat.getColor(this,R.color.black))
+            tvDay.setTextColor(ContextCompat.getColor(this,R.color.black))
+            tv_Date.setTextColor(ContextCompat.getColor(this,R.color.black))
+            tvDate.text="3일 후에 알람이 울려요"
+        }
+        else{
+            tvTime.setTextColor(ContextCompat.getColor(this,R.color.gray))
+            tvDay.setTextColor(ContextCompat.getColor(this,R.color.gray))
+            tv_Date.setTextColor(ContextCompat.getColor(this,R.color.gray))
+            tvDate.text="예정된 알람 없음"
+        }
+
+        swCard.setOnCheckedChangeListener { compoundButton, b ->
+            if (b){ // 활성화
+                tvTime.setTextColor(ContextCompat.getColor(this,R.color.black))
+                tvDay.setTextColor(ContextCompat.getColor(this,R.color.black))
+                tv_Date.setTextColor(ContextCompat.getColor(this,R.color.black))
+                tvDate.text="3일 후에 알람이 울려요"
+            }
+            else{ // 비활성화
+                tvTime.setTextColor(ContextCompat.getColor(this,R.color.gray))
+                tvDay.setTextColor(ContextCompat.getColor(this,R.color.gray))
+                tv_Date.setTextColor(ContextCompat.getColor(this,R.color.gray))
+                tvDate.text="예정된 알람 없음"
+            }
+        }
+
+
+        fabAdd.setOnClickListener{
+            val intent = Intent(this, CreateAlertActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        //getter
+        val tvDateStr= tvDate.text
+        //setter
+        //tvDate.text = "3일 후에 알람이 울려요"
     }
 }
